779dbdcb3dad4047a175f1875ddfa219f5821210
PoiPo2 <kimmc0316@gmail.com>
2021-12-09 10:07:20 +0900

    crawling epidemic script by KMC

--- crawling_epidemic_KMC.py
+++ crawling_epidemic_KMC.py
@@ -17,24 +17,33 @@ def initializeDriverOptions():
 
 
 def crawlData():
+    start_time = time.time()
     driver = initializeDriverOptions()
+    filename = 'disease_epidemic.csv'
+    disease_list = []
+    symptom_list = []
+    items = {'1급': 17, '2급': 22, '3급': 26, '4급': 62}
+
     url = 'https://www.kdca.go.kr/npt/biz/npp/portal/nppSumryMain.do'
     driver.get(url)
-    items = {'1급': 17, '2급': 22, '3급': 26, '4급': 62}
-    disease_list = []
     for i in range(2, 6):
         iteration = items[f'{i - 1}급']
         for j in range(1, iteration + 1):
-            xpath = f'//*[@id="tabs-1"]/div[{i}]/div/div/div/table/tbody/tr[{j}]/td[1]/a'
-            disease_name = driver.find_element_by_xpath(xpath).text.replace('\n', '')
-            driver.find_element_by_xpath(xpath).click()
-            time.sleep(1)
+            disease_xpath = f'//*[@id="tabs-1"]/div[{i}]/div/div/div/table/tbody/tr[{j}]/td[1]/a'
+            disease_name = driver.find_element_by_xpath(disease_xpath).text.replace('\n', '')
+            driver.find_element_by_xpath(disease_xpath).click()
+            # 증상 등 내부 내용 크롤링
+            symptom = driver.find_element_by_xpath('//*[@id="contentDiv"]').text
+            time.sleep(0.5)
             driver.back()
-            time.sleep(1)
-            # n = driver.find_element_by_xpath('//*[@id="contentDiv"]').text
+            time.sleep(0.5)
             disease_list.append(disease_name)
-
-    print(disease_list)
+            symptom_list.append(symptom)
+    # 데이터 프레임 생성
+    dataframe = pd.DataFrame({'Disease': disease_list, 'Symptom': symptom_list})
+    # 파일(.csv) 저장
+    dataframe.to_csv(f'./{filename}', index=False)
+    print(f'"{filename}" is saved.\truntime is {time.time() - start_time:.3f} seconds')
     driver.close()
 
 
