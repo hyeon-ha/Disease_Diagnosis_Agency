4e5ea04d720b62fa4a7928f6207ebe12d20eed8a
Hyeoun <ing02084@naver.com>
2021-12-09 15:50:44 +0900

    change crawling_health_sample.py 3

--- crawling_health_sample.py
+++ crawling_health_sample.py
@@ -31,7 +31,7 @@ try:
                 disease = driver.find_element_by_xpath(disease_name_xpath).text
                 new_tap_url = driver.find_element_by_xpath(disease_name_xpath).get_attribute('href')
                 tap_temp = new_tap_url.split(':')
-                if tap_temp[0] == 'http':  # 새창일 경우
+                if tap_temp[0] in ['http', 'https']:  # 새창일 경우
                     new_tap_url = driver.find_element_by_xpath(disease_name_xpath).get_attribute('href')
                     driver.get(new_tap_url)
                     time.sleep(0.2)
