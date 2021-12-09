a8473c47cd6ca9951d2d372b73e91f895ff6f847
Hyeoun <ing02084@naver.com>
2021-12-09 12:01:27 +0900

    change codes

--- One_sentence.py
+++ One_sentence.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('./crawling_data/naver_movie_reviews_2015_2021.csv')
-one_sentences = []
+df = pd.read_csv('./crawling_data/disease_all_index.csv')
+one_contents = []
 for disease in df['Disease'].unique():
     temp = df[df['Disease'] == disease]
     temp = temp['Content']
     one_sentence = ' '.join(temp)
-    one_sentences.append(one_sentence)
-df_one_sentences = pd.DataFrame({'Disease' : df['Disease'].unique(), 'Content' : one_sentences})
-print(df_one_sentences.head())
-df_one_sentences.to_csv('./crawling_data/Disease_content_onesentence.csv', index=False)
+    one_contents.append(one_sentence)
+df_one_contents = pd.DataFrame({'Disease' : df['Disease'].unique(), 'Content' : one_contents})
+print(df_one_contents.head())
+df_one_contents.to_csv('./crawling_data/Disease_content_onesentence.csv', index=False)
--- preprocessing.py
+++ preprocessing.py
@@ -19,33 +19,36 @@ import re
 # df.to_csv('./crawling_data/cleaned_review_2015_2021.csv', index=False)
 # exit()
 
-df = pd.read_csv('./crawling_data/naver_movie_reviews_onesentence_2015_2021.csv')
+df = pd.read_csv('./crawling_data/Disease_content_onesentence.csv')
 print(df.head())
 
 okt = Okt()
 stopwords = pd.read_csv('./crawling_data/stopwords.csv', index_col=0)
 stopwords = list(stopwords['stopword'])
+add_stopwords = ['암이란', '발생부위', '정의', '종류', '관련통계', '예방', '위험요인', '예방법', '조기검진', '진단', '일반적증상', '진단방법', '진행단계',
+                 '감별진단', '치료', '치료방법', '치료의', '부작용', '재발', '치료현황', '생활가이드', '일상생활', '식생활', '요약설명', '이란']
+stopwords = stopwords + add_stopwords
 
 count = 0
-cleaned_sentences = []
+cleaned_contents = []
 for content in df.Content:
     count += 1
     if count % 10 == 0: print('.', end='')
     if count % 100 == 0: print()
-    content = re.sub('[^가-힣a-zA-Z ]', '', content)  # 한글만 제외하고 나머지는 모두 없앤다.
+    content = re.sub('[^가-힣a-zA-Z ]', '', content)
     token = okt.pos(content, stem=True)
     df_token = pd.DataFrame(token, columns=['word', 'class'])
     df_cleaned_token = df_token[(df_token['class'] == 'Noun') | (df_token['class'] == 'Verb') | (df_token['class'] == 'Adjective')]  # 명사, 동사, 부사만 활용한다.
 
     words = []
     for word in df_cleaned_token['word']:
-        if len(word) > 1 and word not in stopwords: words.append(word)
+        if (len(word) > 1 or word == '암') and word not in stopwords: words.append(word)
 
-    cleaned_sentence = ' '.join(words)
-    cleaned_sentences.append(cleaned_sentence)
+    cleaned_content = ' '.join(words)
+    cleaned_contents.append(cleaned_content)
 
-df['cleaned_sentences'] = cleaned_sentences
+df['cleaned_content'] = cleaned_contents
 print(df.head())
 df.info()
-df = df[['titles', 'cleaned_sentences']]  # == df.drop('reviews', inplace=True, axis=1)
-df.to_csv('./crawling_data/cleaned_review_2015_2021.csv', index=False)
+df = df[['Disease', 'cleaned_content']]
+df.to_csv('./crawling_data/cleaned_disease_content.csv', index=False)
